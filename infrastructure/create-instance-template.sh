90ad628b13c8d3b40b5e668b55b1d75322c6991c
Greg Chan <gregorywarrenchan@gmail.com>
2019-08-13 23:04:56 -0700

    fix(infrastructre): clean up infrastructure scripts

--- infrastructure/create-instance-template.sh
+++ infrastructure/create-instance-template.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+# Specify the BOT_TOKEN and STOCK_API as positional arguments
 gcloud compute instance-templates create-with-container phillip-instance-template \
   --machine-type f1-micro \
   --boot-disk-size 10GB \
   --container-image gcr.io/discord-phillip/phillip:latest \
+  --container-env BOT_TOKEN=$2 \
+  --container-env STOCK_API=$3 \
   --tags http-server,https-server
